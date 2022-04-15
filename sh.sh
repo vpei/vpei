8918ce692304b2bf422d13822724e38e6464ee7b
vpei <vpei@live.com>
2022-04-15 13:31:04 +0800

    Initial commit

--- sh.sh
+++ sh.sh
@@ -3,3 +3,5 @@
 #删除docker无脑初始化教程
 # sed -i '31,39d' package/lean/luci-app-docker/po/zh-cn/docker.po
 # rm -rf lean/luci-app-docker/root/www
+
+git pull && git add -A && git commit -am "Initial commit" && git push
